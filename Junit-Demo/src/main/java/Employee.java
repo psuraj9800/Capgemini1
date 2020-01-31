ea54e25f0b0bb904a58dd770f73e250f82a7fbd0
psuraj9800 <60469121+psuraj9800@users.noreply.github.com>
2020-01-31 15:48:30 +0530

    Add files via upload

---
+++ Junit-Demo/src/main/java/Employee.java
@@ -0,0 +1,19 @@
+
+public class Employee {
+	
+	private int Id;
+	private String Name;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+
+}
---
+++ Junit-Demo/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("Suraj");
+		System.out.println(emp.getId() + emp.getName());
+	}
+
+}
